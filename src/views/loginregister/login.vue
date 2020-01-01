4b93606012621aa42061491dcae03c0dcb15f45b
Deasoso <932152571@qq.com>
2020-01-01 20:28:40 +0800

    fix login/rigister.

--- src/views/loginregister/login.vue
+++ src/views/loginregister/login.vue
@@ -1,45 +1,50 @@
 
 <template>
   <div class="demo-page">
-      <mt-header fixed title="登录">
-          <router-link to="/" slot="left">
+    <mt-header fixed title="登录">
+      <router-link to="/" slot="left">
         <mt-button icon="back">返回</mt-button>
-    </router-link>
+      </router-link>
     <mt-button icon="more" slot="right"></mt-button>
-      </mt-header>
-        <br>
-        <mt-field label="账号" placeholder="请输入手机号码或邮箱" type="email" v-model="email"></mt-field>
-        <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
-        <br>
-<mt-button size="large" type="primary">登录</mt-button>
+    </mt-header>
+    <div class="inputs">
+      <mt-field label="账号" placeholder="请输入手机号码或邮箱" type="email" v-model="email"></mt-field>
+      <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+    </div>
+    <mt-button size="large" type="primary">登录</mt-button>
     <mt-checklist
-        class="check"
-        v-model="value"
-        :options="['记住账号及密码']"> 
+      class="check"
+      v-model="value"
+      :options="['记住账号及密码']"> 
     </mt-checklist>
     <p class="bstyle"><a href="http://localhost:8080/#/register">忘记密码</a></p>
     <p class="cstyle">没有账号？<a href="http://localhost:8080/#/register">立即注册</a></p>
-   
   </div>
 </template>
 <script>
 module.exports = {
   data() {
     return {
-      title: 'MoBan',
-      value: [20, 50],
-    }
+      title: "MoBan",
+      value: [20, 50]
+    };
   },
-  methods:{
-    changename(inputname){
-       this.$store.commit("settitle", {'name': inputname});
+  methods: {
+    changename(inputname) {
+      this.$store.commit("settitle", { name: inputname });
     }
   }
-}
+};
+// 批改：1、注意缩进
+//      2、换行最好不要用<br>，不好控制像素
 </script>
 
 <style scoped>
-.demo-page{
+.demo-page {
 
+}
+.inputs{
+  margin-top: 20px;
+  margin-bottom: 20px;
 }
 </style>
--- src/views/loginregister/register.vue
+++ src/views/loginregister/register.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="demo-page">
-        <mt-header fixed title="注册">
-          <router-link to="/" slot="left">
+    <mt-header fixed title="注册">
+      <router-link to="/" slot="left">
         <mt-button icon="back">返回</mt-button>
-    </router-link>
-    <mt-button icon="more" slot="right"></mt-button>
-      </mt-header>
-   <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
-    <mt-field label="账号" placeholder="请输入邮箱或手机号" type="email" v-model="email"></mt-field>
-    <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
-    <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
-    <mt-field label="验证码" v-model="captcha">
-    <img src="https://profile.csdnimg.cn/8/6/A/1_yflook" height="45px" width="100px">
-    </mt-field>
-    <p class="message">已有账号？<a href="http://localhost:8080/#/login">登陆<span style="color=#3399ff;"></span></a></p>
-    <p class="message1">注册即代表您已阅读并同意《<a href="http://localhost:8080/#/login">使用协议</a>》</p>
-    <br>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <div class="inputs">
+      <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
+      <mt-field label="账号" placeholder="请输入邮箱或手机号" type="email" v-model="email"></mt-field>
+      <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+      <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
+      <mt-field label="验证码" v-model="captcha">
+        <img src="https://profile.csdnimg.cn/8/6/A/1_yflook" height="45px" width="100px">
+      </mt-field>
+    </div>
+    <div class="inputs">
+      已有账号？<a href="http://localhost:8080/#/login">登陆<span style="color=#3399ff;"></span></a>
+    </div>
+    <div class="inputs">
+      注册即代表您已阅读并同意《<a href="http://localhost:8080/#/login">使用协议</a>》
+    </div>
     <mt-button size="large" type="primary">注册</mt-button>
 </div>
 </template>
@@ -34,5 +39,9 @@ module.exports = {
 <style scoped>
 .demo-page{
 
+}
+.inputs{
+  padding-top: 10px;
+  padding-bottom: 10px;
 }
 </style>
