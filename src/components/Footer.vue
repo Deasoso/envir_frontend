da4fdd9cd3d5404d6d208b472f1c7cf17c32fbab
Deasoso <932152571@qq.com>
2020-01-01 17:58:16 +0800

    fix footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,11 +4,11 @@
     :fixed="true">
       <mt-tab-item id="主页" @click.native="menuTab('/Home')">
         <img slot="icon" src="https://img.alicdn.com/imgextra/i4/29928842/O1CN01geMeSm2FBgaXX0rLs_!!0-saturn_solar.jpg_220x220.jpg_.webp">
-        页面1
+        主页
       </mt-tab-item>
-      <mt-tab-item id="资产公告"  @click.native="menuTab('/page1')">
+      <mt-tab-item id="主页"  @click.native="menuTab('/orderlist')">
         <img slot="icon" src="https://img.alicdn.com/imgextra/i4/29928842/O1CN01geMeSm2FBgaXX0rLs_!!0-saturn_solar.jpg_220x220.jpg_.webp">
-        页面2
+        订单
       </mt-tab-item>
       <mt-tab-item id="个人中心" @click.native="menuTab('/personal')">
         <img slot="icon" src="https://img.alicdn.com/imgextra/i4/29928842/O1CN01geMeSm2FBgaXX0rLs_!!0-saturn_solar.jpg_220x220.jpg_.webp">
